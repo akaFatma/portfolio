fc2a2803d34ad900f54071c0bdd9c51b0468293b
akaFatma <mf_salhi@esi.dz>
2024-09-01 00:31:27 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import {BrowserRouter} from 'react-router-dom';
+import './App.css'
+import Navbar from './Components/Navbar'
+import About from './Components/About'
+import Contact from './Components/Contact'
+import Feedback from './Components/Feedback'
+import Hero from './Components/Hero'
+import Works from './Components/Works'
+
+
+
+function App() {
+  
+  return (
+    <BrowserRouter>
+    <div>
+
+    </div>
+    </BrowserRouter>
+
+
+    
+ 
+  )
+}
+
+export default App
