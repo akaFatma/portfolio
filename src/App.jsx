98f76cd339ee7eca8a1f772cfc5fbd8f860d7d67
akaFatma <mf_salhi@esi.dz>
2024-09-01 23:11:23 +0100

    finished navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,35 @@
-import { useState } from 'react'
-import {BrowserRouter} from 'react-router-dom';
-import './App.css'
-import Navbar from './Components/Navbar'
+import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
 import About from './Components/About'
 import Contact from './Components/Contact'
-import Feedback from './Components/Feedback'
+import Experience from './Components/Experience'
 import Hero from './Components/Hero'
+import Navbar from './Components/Navbar'
+import Feedback from './Components/Feedback'
+import Tech from './Components/Tech'
 import Works from './Components/Works'
 
 
-
-function App() {
-  
+const App = () => {
   return (
     <BrowserRouter>
-    <div>
 
-    </div>
-    </BrowserRouter>
-
-
+     <div className="relative z-0 bg-primary">
+      <div className="bg-hero-pattern bg-cover">
+        <Navbar />
+        <Hero />
+      </div>
+      <About/>
+      <Experience/>
+      <Tech />
+      <Works />
+     </div>
+     <div className="relative z-0">
+      <Contact/>
+     </div>
     
- 
+    </BrowserRouter>
+  
   )
 }
 
