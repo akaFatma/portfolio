6ea179fcf9facedec370f9e17b803beefe82fbcf
akaFatma <mf_salhi@esi.dz>
2024-09-02 18:04:57 +0100

    about section needs work

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Tilt } from 'react-tilt' //for tilting the cards
+import {motion} from 'framer-motion'
+import  {styles } from '../styles'
 
 const About = () => {
   return (
-    <div></div>
+    <>
+       <motion.div variants={}>
+        <p className={styles.sectionSubText}> Introduction</p>
+        <h2 className={styles.heroHeadText}>Overview</h2>
+       </motion.div>
+
+    </>
+   
   )
 }
 
--- src/Components/Computer.jsx
+++ src/Components/Computer.jsx
@@ -6,8 +6,9 @@ import { Preload } from '@react-three/drei';
 import { useGLTF } from '@react-three/drei';
 import CanvasLoader from "./Loader";
 
-const Computer = () => {
+const Computer = ({IsMobile}) => {
     const computer = useGLTF('../../desktop_pc/scene.gltf');
+    const position = IsMobile ? [0, -3.3, -2.1] : [0, -3.2, -3];
   return (
     <mesh> 
          <hemisphereLight intensity={1} groundColor={"black"}/>
@@ -15,8 +16,8 @@ const Computer = () => {
          <spotLight intensity={5}  position={[-20, 50, 10]} castShadow  angle={0.3} />
          
          <primitive object={computer.scene} 
-         scale={0.8}
-         position={[0,-3.2,-3]}
+         scale={IsMobile ? 0.7 : 0.8}
+         position={position}
          rotation={[-0.01, -0.2, -0.1]}
          />
 
@@ -24,8 +25,25 @@ const Computer = () => {
   )
 }
 const ComputerCanvas = ()=>{
-  return(
 
+   const [IsMobile , setIsMobile] = useState(false);
+
+   useEffect(()=>{
+      // Add a listener for changes to the screen size
+      const mediaQuery=window.matchMedia('(max-width : 500px)');
+      setIsMobile(mediaQuery.matches);
+
+      const handleMediaQueryChange = (event)=>{
+        setIsMobile(event.matches);
+      }
+      mediaQuery.addEventListener('change',handleMediaQueryChange);
+      // Remove the listener when the component is unmounted
+      return()=>{
+        mediaQuery.removeEventListener('change',handleMediaQueryChange);
+      }
+   },[])
+
+  return(
     <Canvas frameloop='demand'
     shadows
     camera={{position : [20 , 3 , 5],fov : 25}}
@@ -37,7 +55,7 @@ const ComputerCanvas = ()=>{
        maxPolarAngle={Math.PI/2}
        minPolarAngle={Math.PI/2}
        />
-       <Computer/>
+       <Computer IsMobile={IsMobile}/>
         </Suspense>  
         <Preload all/>
     </Canvas>
