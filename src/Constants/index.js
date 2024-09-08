39331f107b179f3217fc8f6e8601f8ff3a99f2c8
akaFatma <mf_salhi@esi.dz>
2024-09-08 20:21:23 +0100

    added techologies

--- src/Constants/index.js
+++ src/Constants/index.js
@@ -221,7 +221,7 @@ import {
     {
       name: "Job IT",
       description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        "hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh",
       tags: [
         {
           name: "react",
